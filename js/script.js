7c99f91ea39d7a1a7a69dd3fa7a52391a87ed726
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-21 14:09:20 +0100

    add first script

--- js/script.js
+++ js/script.js
@@ -2,7 +2,41 @@
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
 
+const bikes = [
+    {
+        name: 'One',
+        weight: 5
+    },
+    {
+        name: 'Two',
+        weight: 7
+    },
+    {
+        name: 'Three',
+        weight: 10
+    },
+    {
+        name: 'Four',
+        weight: 12
+    },
+    {
+        name: 'Five',
+        weight: 3
+    }
+]
 
+let minorWeight = 200
+let minorBike = null
+
+for (let i = 0; i < bikes.length; i++) {
+
+    if (bikes[i].weight <= minorWeight) {
+        minorWeight = bikes[i].weight
+        minorBike = bikes[i].name
+    }
+}
+
+console.log(minorBike);
 
 // Snack2
 // Creare un array di oggetti di squadre di calcio.
